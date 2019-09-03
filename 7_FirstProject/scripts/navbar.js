3e64a82f64ec0766d68754ae2610b1021cde015a
Ricardo Figueroa <rfigueroa@intersysconsulting.com>
2019-09-03 16:28:51 -0500

    Finished Burgerie project

---
+++ 7_FirstProject/scripts/navbar.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+  document.getElementById("navbar").classList.add("showNav");
+}
+
+function closeNavbar() {
+  document.getElementById("navbar").classList.remove("showNav");
+}
